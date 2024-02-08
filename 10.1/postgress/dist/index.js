c2b719b7cb22b3067bb1acb04575a20a73224dd4
Sagar Yadav <sagardxd5@gmail.com>
2024-02-08 20:50:19 +0530

    update user query

--- 10.1/postgress/dist/index.js
+++ 10.1/postgress/dist/index.js
@@ -14,37 +14,44 @@ const client = new pg_1.Client({
     connectionString: "postgresql://sagarydv1298:Rjzv6ibCnXg3@ep-sweet-fire-a5xs39kd.us-east-2.aws.neon.tech/neondb?sslmode=require"
 });
 // Creating the sql table
-// async function createUsersTable() {
-//     await client.connect()
-//     const result = await client.query(`
-//     CREATE TABLE users (
-//         id SERIAL PRIMARY KEY,
-//         username VARCHAR(50) UNIQUE NOT NULL,
-//         email VARCHAR(255) UNIQUE NOT NULL,
-//         password VARCHAR(255) NOT NULL,
-//         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-//     );
-//     `)
-//     console.log(result); }
+function createUsersTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        const result = yield client.query(`
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+    `);
+        console.log(result);
+    });
+}
 // createUsersTable();
 //  Inserting a row in a table ( its the wrong way )  
-// async function insertInUsersTable() {
-//     await client.connect()
-//     const result = await client.query(`
-//         INSERT INTO users (username, email, password)
-//         VALUES ('username_here', 'user@example.com', 'user_password');
-//     `)
-//     console.log(result);
-// }
+function insertInUsersTable() {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        const result = yield client.query(`
+        INSERT INTO users (username, email, password)
+        VALUES ('username_here', 'user@example.com', 'user_password');
+    `);
+        console.log(result);
+    });
+}
 // insertInUsersTable()
 // inserting the right way
-// async function insertInUserTable(username: string, email: string, password: string) {
-//     await client.connect()
-//     const query = "INSERT INTO users (username, email, password) VALUES ($1, $2 , $3)";
-//     const values = [username, email , password];
-//     const res = await client.query(query, values)
-//     console.log(res);
-// }
+function insertInUserTable(username, email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        const query = "INSERT INTO users (username, email, password) VALUES ($1, $2 , $3)";
+        const values = [username, email, password];
+        const res = yield client.query(query, values);
+        console.log(res);
+    });
+}
 // insertInUserTable('sagar', 'sagar1@gmail.com', 'sagar')
 // query data in db
 function getuser(username) {
@@ -56,4 +63,15 @@ function getuser(username) {
         console.log(res);
     });
 }
-getuser('snorlxz');
+// getuser('snorlxz')
+//updating in user table
+function updateUserEmail(email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield client.connect();
+        const updateQuery = "UPDATE users SET password=$1 WHERE email=$2";
+        const values = [password, email];
+        const res = yield client.query(updateQuery, values);
+        console.log(res);
+    });
+}
+updateUserEmail('sagar@gmail.com', 'rand');
--- 10.1/postgress/src/index.ts
+++ 10.1/postgress/src/index.ts
@@ -5,44 +5,45 @@ const client = new Client({
 })
 
 // Creating the sql table
-// async function createUsersTable() {
-//     await client.connect()
-//     const result = await client.query(`
-//     CREATE TABLE users (
-//         id SERIAL PRIMARY KEY,
-//         username VARCHAR(50) UNIQUE NOT NULL,
-//         email VARCHAR(255) UNIQUE NOT NULL,
-//         password VARCHAR(255) NOT NULL,
-//         created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-//     );
-//     `)
+async function createUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    );
+    `)
     
-//     console.log(result); }
+    console.log(result); }
 // createUsersTable();
 
 
 //  Inserting a row in a table ( its the wrong way )  
-// async function insertInUsersTable() {
-//     await client.connect()
-//     const result = await client.query(`
-//         INSERT INTO users (username, email, password)
-//         VALUES ('username_here', 'user@example.com', 'user_password');
-//     `)
+async function insertInUsersTable() {
+    await client.connect()
+    const result = await client.query(`
+        INSERT INTO users (username, email, password)
+        VALUES ('username_here', 'user@example.com', 'user_password');
+    `)
 
-//     console.log(result);
-// }
+    console.log(result);
+}
 // insertInUsersTable()
 
 
 // inserting the right way
-// async function insertInUserTable(username: string, email: string, password: string) {
-//     await client.connect()
-//     const query = "INSERT INTO users (username, email, password) VALUES ($1, $2 , $3)";
-//     const values = [username, email , password];
-//     const res = await client.query(query, values)
-//     console.log(res);
-// }
+async function insertInUserTable(username: string, email: string, password: string) {
+    await client.connect()
+    const query = "INSERT INTO users (username, email, password) VALUES ($1, $2 , $3)";
+    const values = [username, email , password];
+    const res = await client.query(query, values)
+    console.log(res);
+}
 // insertInUserTable('sagar', 'sagar1@gmail.com', 'sagar')
+
 
 // query data in db
 async function getuser(username: string) {
@@ -50,6 +51,18 @@ async function getuser(username: string) {
     const query = "SELECT * FROM users WHERE username = $1"
     const values = [username];
     const res = await client.query(query, values)
-    console.log(res)
+    console.log(res);
 }
-getuser('snorlxz')
+// getuser('snorlxz')
+
+
+//updating in user table
+async function updateUserEmail(email: string, password: string) {
+    await client.connect()
+    const updateQuery = "UPDATE users SET password=$1 WHERE email=$2"
+    const values = [password,email]
+    const res = await client.query(updateQuery, values);
+    console.log(res);
+}
+
+updateUserEmail('sagar@gmail.com', 'rand')
