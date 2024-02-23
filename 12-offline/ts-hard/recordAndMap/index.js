04bc0ec7ad6aaea9f52974fb3322cc3dfdaa36aa
Sagar Yadav <sagardxd5@gmail.com>
2024-02-23 06:00:24 +0530

    map ts-hard

--- 12-offline/ts-hard/recordAndMap/index.js
+++ 12-offline/ts-hard/recordAndMap/index.js
@@ -1,6 +1,9 @@
 "use strict";
-const users = {
-    'abc123': { id: 'abc123', name: 'John Doe' },
-    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
-};
-users['abc123'];
+// interface User {
+//     id: string;
+//     name: string;
+// }
+//initializing a empty map
+const users = new Map();
+users.set("1", { id: "1", name: "sagar" });
+console.log(users.get("1"));
--- 12-offline/ts-hard/recordAndMap/index.ts
+++ 12-offline/ts-hard/recordAndMap/index.ts
@@ -1,14 +1,27 @@
+// interface User {
+//     id: string;
+//     name: string;
+// }
+
+// //using record for cleaner syntax
+// type Users = Record<string, User>
+
+// const users: Users = {
+//     'abc123': { id: 'abc123', name: 'John Doe' },
+//     'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+// };
+
+// users['abc123']
+
 interface User {
     id: string;
     name: string;
 }
 
-//using record for cleaner syntax
-type Users = Record<string, User>
+//initializing a empty map
+const users = new Map<string, User>();
+users.set("1", {id: "1", name: "sagar"})
 
-const users: Users = {
-    'abc123': { id: 'abc123', name: 'John Doe' },
-    'xyz789': { id: 'xyz789', name: 'Jane Doe' },
-};
+console.log(users.get("1"));
 
-users['abc123']
+
