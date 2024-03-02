9b361a6396ee729190f3cf4116aeff61369e5233
Sagar Yadav <sagardxd5@gmail.com>
2024-03-02 20:53:47 +0530

    nextjs class

---
+++ 14.1/next-app/app/auth/layout.tsx
@@ -0,0 +1,5 @@
+export default function ({children}: {children: React.ReactNode}) {
+    return (
+<div className="border">20 percent off bro</div>
+    );
+}
---
+++ 14.1/next-app/app/signup/page.tsx
@@ -0,0 +1,7 @@
+
+export default function Home() {
+    return (
+      <div>Sign Up nigga</div>
+    );
+  }
+  
